49216787942020d49eef919058e00cb52e3a50f5
ianngiaw <ian@u.nus.edu>
2015-02-25 18:57:46 +0800

    Edited TestCommand to follow standard

--- TextBuddy/src/com/textbuddy/test/TestCommand.java
+++ TextBuddy/src/com/textbuddy/test/TestCommand.java
@@ -8,73 +8,61 @@ import com.textbuddy.main.Command;
 
 
 public class TestCommand {
-
-	@Test
-	public void testParseCommand() {
-		final Command addCommand = Command.parseCommand("add line");
-		assertEquals(Command.CommandType.ADD, addCommand.getCommandType());
-		assertEquals("line", addCommand.getCommandArgument());
-		
-		final Command addBlankCommand = Command.parseCommand("add");
-		assertEquals(Command.CommandType.ADD, addBlankCommand.getCommandType());
-		assertEquals("", addBlankCommand.getCommandArgument());
-		
-		final Command clearCommand = Command.parseCommand("clear");
-		assertEquals(Command.CommandType.CLEAR, clearCommand.getCommandType());
-		
-		final Command displayCommand = Command.parseCommand("display");
-		assertEquals(Command.CommandType.DISPLAY, displayCommand.getCommandType());
-
-		final Command deleteCommand = Command.parseCommand("delete 1");
-		assertEquals(Command.CommandType.DELETE, deleteCommand.getCommandType());
-		assertEquals("1", deleteCommand.getCommandArgument());
-		
-		final Command exitCommand = Command.parseCommand("exit");
-		assertEquals(Command.CommandType.EXIT, exitCommand.getCommandType());
-		
-		final Command invalidBlankCommand = Command.parseCommand("");
-		assertEquals(Command.CommandType.INVALID, invalidBlankCommand.getCommandType());
-		
-		final Command invalidSpacedCommand = Command.parseCommand(" ");
-		assertEquals(Command.CommandType.INVALID, invalidSpacedCommand.getCommandType());
-		
-		final Command invalidTypedCommand = Command.parseCommand("alksdjlak askldja");
-		assertEquals(Command.CommandType.INVALID, invalidTypedCommand.getCommandType());
-	}
 	
 	@Test
-	public void testParseSearchCommand () {
+	public void testParseSearchBlankCommand () {
 		final Command searchBlankCommand = Command.parseCommand("search");
 		assertEquals(Command.CommandType.SEARCH, searchBlankCommand.getCommandType());
 		assertEquals("", searchBlankCommand.getCommandArgument());
-
+	}
+	
+	@Test
+	public void testParseSearchSpaceCommand () {
 		final Command searchSpacedCommand = Command.parseCommand("search     ");
 		assertEquals(Command.CommandType.SEARCH, searchSpacedCommand.getCommandType());
 		assertEquals("", searchSpacedCommand.getCommandArgument());
-
+	}
+	
+	@Test
+	public void testParseSearch123Command () {
 		final Command search123Command = Command.parseCommand("search 123");
 		assertEquals(Command.CommandType.SEARCH, search123Command.getCommandType());
 		assertEquals("123", search123Command.getCommandArgument());
-		
+	}
+	
+	@Test
+	public void testParseSearchWrongCaseCommand () {
 		final Command searchWrongCseCommand = Command.parseCommand("SeArCh");
 		assertEquals(Command.CommandType.INVALID, searchWrongCseCommand.getCommandType());
 	}
 	
 	@Test
-	public void testParseSortCommand () {
-		final Command sortCommand = Command.parseCommand("sort");
-		assertEquals(Command.CommandType.SORT, sortCommand.getCommandType());
-		
-		final Command sortFrontSpaceCommand = Command.parseCommand("     sort");
-		assertEquals(Command.CommandType.SORT, sortFrontSpaceCommand.getCommandType());
-
-		final Command sortEndSpaceCommand = Command.parseCommand("sort     ");
-		assertEquals(Command.CommandType.SORT, sortEndSpaceCommand.getCommandType());
-		
-		final Command sortBothSpaceCommand = Command.parseCommand("     sort     ");
-		assertEquals(Command.CommandType.SORT, sortBothSpaceCommand.getCommandType());
-		
-		final Command sortWrongCaseCommand = Command.parseCommand("SoRt");
-		assertEquals(Command.CommandType.INVALID, sortWrongCaseCommand.getCommandType());
+	public void testParseSortNormal () {
+		final Command sortNormal = Command.parseCommand("sort");
+		assertEquals(Command.CommandType.SORT, sortNormal.getCommandType());
+	}
+	
+	@Test
+	public void testParseSortFrontSpace () {
+		final Command sortFrontSpace = Command.parseCommand("     sort");
+		assertEquals(Command.CommandType.SORT, sortFrontSpace.getCommandType());
+	}
+	
+	@Test
+	public void testParseSortEndSpace () {
+		final Command sortEndSpace = Command.parseCommand("sort     ");
+		assertEquals(Command.CommandType.SORT, sortEndSpace.getCommandType());
+	}
+	
+	@Test
+	public void testParseSortBothSpace () {
+		final Command sortBothSpace = Command.parseCommand("     sort     ");
+		assertEquals(Command.CommandType.SORT, sortBothSpace.getCommandType());
+	}
+	
+	@Test
+	public void testParseSortWrongCase () {
+		final Command sortWrongCase = Command.parseCommand("SoRt");
+		assertEquals(Command.CommandType.INVALID, sortWrongCase.getCommandType());
 	}
 }
