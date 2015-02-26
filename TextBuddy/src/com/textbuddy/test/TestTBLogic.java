b9e07e2814cf44d77dda3916d2a6dd5d4af4e034
ianngiaw <ian@u.nus.edu>
2015-02-26 15:17:45 +0800

    Editted TestTBLogic to follow standard

--- TextBuddy/src/com/textbuddy/test/TestTBLogic.java
+++ TextBuddy/src/com/textbuddy/test/TestTBLogic.java
@@ -2,43 +2,17 @@ package com.textbuddy.test;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.textbuddy.main.TextBuddyLogic;
 
 public class TestTBLogic {
-	TextBuddyLogic logic = new TextBuddyLogic("mytextfile.txt");
+	TextBuddyLogic logic = new TextBuddyLogic("testTBLogic.txt");
 	
-	@Test
-	public void testExecuteSearchCommand () {
-		logic.executeCommand("clear");
-		
-		logic.executeCommand("add line1");
-		logic.executeCommand("add line2");
-		logic.executeCommand("add line3");
-		
-		String searchBlankResponse = logic.executeCommand("search");
-		assertEquals("no search terms", searchBlankResponse);
-		
-		String searchNoMatchResponse = logic.executeCommand("search z");
-		assertEquals("no match found for \"z\" in mytextfile.txt", searchNoMatchResponse);
-		
-		String searchLineResponse = logic.executeCommand("search line");
-		assertEquals("1. line1\n2. line2\n3. line3", searchLineResponse);
-
-		String search2Response = logic.executeCommand("search 2");
-		assertEquals("2. line2", search2Response);
-		
-		logic.executeCommand("clear");
-	}
-	
-	@Test
-	public void testExecuteSortCommand () {
-		logic.executeCommand("clear");
-		
-		String sortEmptyResponse = logic.executeCommand("sort");
-		assertEquals("mytextfile.txt is empty", sortEmptyResponse);
-		
+	@Before
+	public void setupLogic () {
 		logic.executeCommand("add d");
 		logic.executeCommand("add c");
 		logic.executeCommand("add b");
@@ -47,10 +21,47 @@ public class TestTBLogic {
 		logic.executeCommand("add aa");
 		logic.executeCommand("add ad");
 		logic.executeCommand("add ac");
-		
-		String sortResponse = logic.executeCommand("sort");
-		assertEquals("mytextfile.txt has been sorted alphabetically", sortResponse);
-		
+	}
+	
+	@After
+	public void tearDownLogic () {
 		logic.executeCommand("clear");
+	}
+	
+	@Test
+	public void testExecuteCommandSearchBlank () {
+		String searchBlankResponse = logic.executeCommand("search");
+		assertEquals("no search terms", searchBlankResponse);
+	}
+	
+	@Test
+	public void testExecuteCommandSearchZ () {
+		String searchZResponse = logic.executeCommand("search z");
+		assertEquals("no match found for \"z\" in testTBLogic.txt", searchZResponse);
+	}
+	
+	@Test
+	public void testExecuteCommandSearchD () {
+		String searchDResponse = logic.executeCommand("search d");
+		assertEquals("1. d\n7. ad", searchDResponse);
+	}
+	
+	@Test
+	public void testExecuteCommandSearchAa () {
+		String searchAaResponse = logic.executeCommand("search aa");
+		assertEquals("6. aa", searchAaResponse);
+	}
+	
+	@Test
+	public void testExecuteCommandSortEmpty () {
+		logic.executeCommand("clear");
+		String sortEmptyResponse = logic.executeCommand("sort");
+		assertEquals("testTBLogic.txt is empty", sortEmptyResponse);
+	}
+	
+	@Test
+	public void testExecuteCommandSortFilled () {
+		String sortResponse = logic.executeCommand("sort");
+		assertEquals("testTBLogic.txt has been sorted alphabetically", sortResponse);
 	}
 }
