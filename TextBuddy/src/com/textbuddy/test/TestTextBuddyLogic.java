30a4ae1ce2e2da118687f73dcf9454f40be915ba
ianngiaw <ian@u.nus.edu>
2015-02-26 15:51:50 +0800

    Optimised imports in TestTextBuddyLogic

--- TextBuddy/src/com/textbuddy/test/TestTextBuddyLogic.java
+++ TextBuddy/src/com/textbuddy/test/TestTextBuddyLogic.java
@@ -4,8 +4,6 @@ import static org.junit.Assert.assertEquals;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
